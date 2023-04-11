a0fdaf8a3eb18c4a3889704bea9f12f8731cca44
Devisha Gupta <devisha18.003gupta@gmail.com>
2023-04-12 01:22:45 +0530

    changes in Safe Home

--- lib/widgets/home_widgets/SafeHome/SafeHome.dart
+++ lib/widgets/home_widgets/SafeHome/SafeHome.dart
@@ -1,35 +1,51 @@
 import 'package:flutter/material.dart';
 
 class SafeHome extends StatelessWidget {
-  const SafeHome({super.key});
+  showModelSfaeHome(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+            ),
+            child: Card());
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Container(
-        height: 180,
-        width: MediaQuery.of(context).size.width * 0.7,
-        decoration: BoxDecoration(),
-        child: Row(
-          children: [
-            Expanded(
-                child: Column(
+    return InkWell(
+        onTap: () => showModelSfaeHome(context),
+        child: Card(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Container(
+            height: 180,
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(),
+            child: Row(
               children: [
-                ListTile(
-                  title: Text("Send Location"),
-                )
+                Expanded(
+                    child: Column(
+                  children: [
+                    ListTile(
+                      title: Text("Send Location"),
+                    ),
+                  ],
+                )),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset("assets/Map2_WSA.jpg")),
               ],
-            )),
-            ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset("assets/Map2_WSA.jpg"))
-          ],
-        ),
-      ),
-    );
+            ),
+          ),
+        ));
   }
 }
