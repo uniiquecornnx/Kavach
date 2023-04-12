ead5672a15de255fb633a648092fa140527f8654
Samarth <samkusaxena@gmail.com>
2023-04-12 12:41:42 +0530

    added uderlogin

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,11 +1,33 @@
+import 'package:first_app/components/custom_textfield.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              "User Login",
+              style: TextStyle(fontSize: 40),
+            ),
+            Image.asset(
+              'assets/logo.png',
+              height: 50,
+              width: 60,
+            ),
+            CustomTextField(
+              hintText: "enter name",
+              prefix: Icon(Icons.person),
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
