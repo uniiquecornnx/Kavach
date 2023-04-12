6a3d7c1763a1c28d8197edbbe3318b831caca1ff
Samarth <samkusaxena@gmail.com>
2023-04-12 12:07:15 +0530

    added new file and changes in login screen

---
+++ lib/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:first_app/home_screen.dart';
+import 'package:first_app/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,6 +22,6 @@ class MyApp extends StatelessWidget {
           ),
           primarySwatch: Colors.blue,
         ),
-        home: HomeScreen());
+        home: LoginScreen());
   }
 }
